b1552bc131063f338005ec53b2d11e411820ea52
saidqb <saidqb@gmail.com>
2024-07-17 13:04:47 +0700

    update: add php unit test

--- src/Lib/Str.php
+++ src/Lib/Str.php
@@ -4,4 +4,10 @@ namespace Saidqb\CorePhp\Lib;
 
 class Str
 {
+
+
+    public function toUpper($str)
+    {
+        return strtoupper($str);
+    }
 }
---
+++ tests/Strtest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Saidqb\CorePhp\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Saidqb\CorePhp\Lib\Str;
+
+/**
+ * @covers \Saidqb\CorePhp\Lib\Str
+ * running test
+ * ./vendor/bin/phpunit tests/Strtest.php
+ */
+class Strtest extends TestCase
+{
+    public function testSample()
+    {
+        $t = new Str();
+        $this->assertSame('TEST', $t->toUpper('test'));
+    }
+}
